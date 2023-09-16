b919dfa86fb4d23cd7586c9163df23d4cd738c54
Chitang Chin <chitangb.chin@gmail.com>
2023-09-15 22:37:27 -0400

    Completed Exercise

--- Program.cs
+++ Program.cs
@@ -1,33 +1,23 @@
-﻿try
-{
-    Process1();
-}
-catch
-{
-    Console.WriteLine("An exception has occurred");
-}
+﻿// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[]{"three", "9999999999", "0", "2" };
 
-Console.WriteLine("Exit program");
-
-static void Process1()
+foreach (string inputValue in inputValues)
 {
+    int numValue = 0;
     try
     {
-        WriteMessage();
+        numValue = int.Parse(inputValue);
     }
-    catch
+    catch (FormatException)
     {
-        Console.WriteLine("Exception caught in Process1");
+        Console.WriteLine("Invalid readResult. Please enter a valid number.");
     }
-}
-
-static void WriteMessage()
-{
-    double float1 = 3000.0;
-    double float2 = 0.0;
-    int number1 = 3000;
-    int number2 = 0;
-
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 }
