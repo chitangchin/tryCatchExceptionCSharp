c7c608ea39e198fc0a545586aa4ef57b71c4b0fa
Chitang Chin <chitangb.chin@gmail.com>
2023-09-15 22:21:34 -0400

    Completed Exercise

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+try
+{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+catch
+{
+    Console.WriteLine("An exception has been caught");
+}
+
+Console.WriteLine("Exit program");
